c0163cd47cdcdec196cae1629ebf560abe7fa985
Jon Fuller <Garconis@users.noreply.github.com>
2022-01-05 14:30:17 -0500

    Update functions.php
    Updated by adding new functions to show Library Item Shortcode column to the Divi Library admin page table.

--- functions.php
+++ functions.php
@@ -33,6 +33,22 @@ function showmodule_shortcode($moduleid) {
 }
 add_shortcode('showmodule', 'showmodule_shortcode');
 
+// Adds new admin column to show the shortcode ID in the Divi Library page table
+function fs_create_shortcode_column( $columns ) {
+	$columns['fs_shortcode_id'] = 'Library Item Shortcode';
+	return $columns;
+}
+// Display shortcode column info on Divi Library page table
+function fs_shortcode_column_content( $column, $id ) {
+	if( 'fs_shortcode_id' == $column ) {
+		echo '<p>[showmodule id="' . $id . '"]</p>';
+	}
+}
+// create new shortcode column in et_pb_layout screen
+add_filter( 'manage_et_pb_layout_posts_columns', 'fs_create_shortcode_column', 5 );
+// add the shortcode content to the new column
+add_action( 'manage_et_pb_layout_posts_custom_column', 'fs_shortcode_column_content', 5, 2 );
+
 /* Gravity Forms */
 
 // This filter can be used to prevent the page from auto jumping to form confirmation upon form submission
